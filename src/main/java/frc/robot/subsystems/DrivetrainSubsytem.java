8293d5d795753a8fabc25d7a82eff7684e785756
SuleymanSade <suleymansade09@gmail.com>
2024-12-07 14:01:54 -0800

    Initial Commit

---
+++ src/main/java/frc/robot/subsystems/DrivetrainSubsytem.java
@@ -0,0 +1,38 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import edu.wpi.first.cscore.VideoSource.ConnectionStrategy;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.RobotContainer;
+
+public class DrivetrainSubsytem extends SubsystemBase {
+  /** Creates a new DrivetrainSubsytem. */
+  CANSparkMax frontLeftMotor;
+  CANSparkMax frontRightMotor;
+  CANSparkMax backLeftMotor;
+  CANSparkMax backRightMotor;
+
+  public DrivetrainSubsytem() {
+    frontLeftMotor = new CANSparkMax(Constants.FRONT_LEFT_MOTOR_ID, MotorType.kBrushless);
+    frontRightMotor = new CANSparkMax(Constants.FRONT_RIGHT_MOTOR_ID, MotorType.kBrushless);
+    backLeftMotor = new CANSparkMax(Constants.BACK_LEFT_MOTOR_ID, MotorType.kBrushless);
+    backRightMotor = new CANSparkMax(Constants.BACK_RIGHT_MOTOR_ID, MotorType.kBrushless);
+
+    backLeftMotor.follow(frontLeftMotor);
+    backRightMotor.follow(frontRightMotor);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+    frontLeftMotor.set(RobotContainer.controler.getY() * -0.5);
+    frontRightMotor.set(RobotContainer.controler.getThrottle() * 0.5);
+  }
+}
