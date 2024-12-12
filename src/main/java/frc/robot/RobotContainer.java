c05add323c3ec76c7ad67d3ca7dcc3c62fd48706
Lakemonsters2635 <lakemonsters.software.team@gmail.com>
2024-12-11 18:49:46 -0800

    Working code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,17 +4,14 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.Commands.LeafBlowerCommand;
 import frc.robot.subsystems.DrivetrainSubsytem;
 import frc.robot.subsystems.LeafBlowerSubsystem;
-import frc.robot.subsystems.Commands.LeafBlowerCommand;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -62,7 +59,7 @@ public class RobotContainer {
 
 
     //buttons
-    leafBlowerButton.whileTrue(m_leafBlowerCommand);
+    leafBlowerButton.toggleOnTrue(m_leafBlowerCommand);
 
     // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
 
