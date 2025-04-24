ac6b20790e88d31e7f7b61995242c7ef68f5f3b8
Lakemonsters2635 <lakemonsters.software.team@gmail.com>
2025-04-23 20:14:01 -0700

    added deadbead and dpad precise functionality

--- src/main/java/frc/robot/subsystems/DrivetrainSubsytem.java
+++ src/main/java/frc/robot/subsystems/DrivetrainSubsytem.java
@@ -5,8 +5,10 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.Robot;
@@ -19,11 +21,21 @@ public class DrivetrainSubsytem extends SubsystemBase {
   CANSparkMax backLeftMotor;
   CANSparkMax backRightMotor;
 
+  double leftMotorSpeedX = 0;
+  double rightMotorSpeedX = 0;
+  double leftMotorSpeedY = 0;
+  double rightMotorSpeedY = 0;
+
   public DrivetrainSubsytem() {
     frontLeftMotor = new CANSparkMax(Constants.FRONT_LEFT_MOTOR_ID, MotorType.kBrushless);
     frontRightMotor = new CANSparkMax(Constants.FRONT_RIGHT_MOTOR_ID, MotorType.kBrushless);
     backLeftMotor = new CANSparkMax(Constants.BACK_LEFT_MOTOR_ID, MotorType.kBrushless);
     backRightMotor = new CANSparkMax(Constants.BACK_RIGHT_MOTOR_ID, MotorType.kBrushless);
+
+    frontLeftMotor.setIdleMode(IdleMode.kBrake);
+    frontRightMotor.setIdleMode(IdleMode.kBrake);
+    backLeftMotor.setIdleMode(IdleMode.kBrake);
+    backRightMotor.setIdleMode(IdleMode.kBrake);
 
     backLeftMotor.follow(frontLeftMotor);
     backRightMotor.follow(frontRightMotor);
@@ -32,8 +44,46 @@ public class DrivetrainSubsytem extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    frontLeftMotor.set( (RobotContainer.controler.getLeftY() * -.5) + RobotContainer.controler.getLeftX() * .5);
-    frontRightMotor.set((RobotContainer.controler.getLeftY() * .5)  + RobotContainer.controler.getLeftX() * .5);
+    if(Math.abs(RobotContainer.controler.getRightX()) > 0.25){
+      leftMotorSpeedX = RobotContainer.controler.getRightX() * .5;
+      rightMotorSpeedX  = RobotContainer.controler.getRightX() * .5;
+    }
+    else{
+      leftMotorSpeedX = 0;
+      rightMotorSpeedX = 0;
+    }
     
+    if(Math.abs(RobotContainer.controler.getRightY()) > 0.25){
+      leftMotorSpeedY = RobotContainer.controler.getRightY() * -.5;
+      rightMotorSpeedY = RobotContainer.controler.getRightY() * .5;
+    }
+    else{
+      leftMotorSpeedY = 0;
+      rightMotorSpeedY = 0;
+    }    
+    frontLeftMotor.set(leftMotorSpeedX + leftMotorSpeedY);
+    frontRightMotor.set(rightMotorSpeedX + rightMotorSpeedY);
+
+      // frontLeftMotor.set((RobotContainer.controler.getRightY() * -.5) + RobotContainer.controler.getRightX() * .5);
+      // frontRightMotor.set((RobotContainer.controler.getRightY() * .5)  + RobotContainer.controler.getRightX() * .5);
+    
+
+    if(RobotContainer.controler.getPOV() == 0) {
+      frontLeftMotor.set(0.25);
+      frontRightMotor.set(0.25 * -1);
+    }
+    else if(RobotContainer.controler.getPOV() == 90){
+      frontLeftMotor.set(0.25);
+      frontRightMotor.set(.25);
+    }
+    else if(RobotContainer.controler.getPOV() == 180){
+      frontLeftMotor.set(-0.25);
+      frontRightMotor.set(-0.25 * -1);
+    }
+    else if(RobotContainer.controler.getPOV() == 270){
+      frontLeftMotor.set(-0.25);
+      frontRightMotor.set(-0.25);
+    }
+  
   }
 }
